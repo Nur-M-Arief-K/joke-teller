7eb2b74e5c32c88695c2b9166787882f1bbf1c62
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-12-07 09:52:39 +0700

    add joke api

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ function test() {
 };
 
 test();
+
+// get jokes from joke API
+async function getJokes() {
+    let joke = "";
+    const apiURL = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        };
+    } catch (error) {
+        
+    }
+}
+
+getJokes();
